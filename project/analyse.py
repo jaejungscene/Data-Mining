c81c578feb3e9acdb27297401a5006f1d86777f9
jaejung <ljj0512@gmail.com>
2022-10-04 15:13:30 +0900

    all commit

---
+++ project/analyse.py
@@ -0,0 +1,4 @@
+print("here analyze part")
+# with open("./test.txt", "w") as f:
+#     f.write("hello world!")
+
---
+++ project/backend/server.js
@@ -0,0 +1,3 @@
+var spawn = require("child_process").spawn;
+
+const result = spawn("py",["../analyse.py"]);
