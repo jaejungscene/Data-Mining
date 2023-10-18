55ba468f9eac30fedccf8c61df6588e9164b615f
jaejungscene <ljj0512@gmail.com>
2023-10-18 10:30:07 +0900

    update file structure

--- Project/backend/insert_foreign-book.py
+++ Project/backend/insert_foreign-book.py
@@ -3,41 +3,42 @@
 import pandas as pd
 import sqlite3
 
-conn = sqlite3.connect("./Books.db")
-cur = conn.cursor()
-conn.execute("""
-        CREATE TABLE Foreign_book(
-            id INTEGER,
-            title TEXT,
-            genres TEXT,
-            author TEXT,
-            rating REAL, 
-            publisher TEXT, 
-            p_date INTEGER, 
-            pages INTEGER, 
-            language TEXT,
-            description TEXT, 
-            imgUrl TEXT
+def insert_foreign_books():
+    conn = sqlite3.connect("./data/Books.db")
+    cur = conn.cursor()
+    conn.execute("""
+            CREATE TABLE Foreign_book(
+                id INTEGER,
+                title TEXT,
+                genres TEXT,
+                author TEXT,
+                rating REAL, 
+                publisher TEXT, 
+                p_date INTEGER, 
+                pages INTEGER, 
+                language TEXT,
+                description TEXT, 
+                imgUrl TEXT
+            )
+    """)
+
+    data = pd.read_csv("./data/foreign-books.csv")
+    for i in range(len(data)):
+        title = data["title"].iloc[i]
+        genre_list = data["genres"].iloc[i].replace("[","").replace("]","").replace("\'","")
+        author = data["author"].iloc[i]
+        rating = data["rating"].iloc[i]
+        publisher = data["publisher"].iloc[i]
+        p_date = data["publishDate"].iloc[i]
+        pages = data["pages"].iloc[i]
+        language = data["language"].iloc[i]
+        description = data["description"][i]
+        imgUrl = data["coverImg"][i]
+        cur.execute(
+            'INSERT INTO Foreign_book VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            (i, title, genre_list, author, rating, publisher, p_date, pages, language, description, imgUrl)
         )
-""")
+        print(i, title)
+        conn.commit()
 
-
-data = pd.read_csv("./data/foreign-books.csv")
-for i in range(len(data)):
-    title = data["title"].iloc[i]
-    genre_list = data["genres"].iloc[i].replace("[","").replace("]","").replace("\'","")
-    author = data["author"].iloc[i]
-    rating = data["rating"].iloc[i]
-    publisher = data["publisher"].iloc[i]
-    p_date = data["publishDate"].iloc[i]
-    pages = data["pages"].iloc[i]
-    language = data["language"].iloc[i]
-    description = data["description"][i]
-    imgUrl = data["coverImg"][i]
-    cur.execute(
-        'INSERT INTO Foreign_book VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        (i, title, genre_list, author, rating, publisher, p_date, pages, language, description, imgUrl)
-    )
-    print(i, title)
-    conn.commit()
-conn.close()
+    conn.close()
